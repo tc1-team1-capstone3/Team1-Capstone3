3b3a0c3bc85ba8240ea49a733015104434b9e91a
Adam Rice <adamdanielrice1@gmail.com>
2020-07-31 14:16:42 -0500

    Added MongoDB, Made Model and dao.

--- Capstone3/src/main/java/com/AllForPets/PetsForAll/Capstone3/Models/ProductModel.java
+++ Capstone3/src/main/java/com/AllForPets/PetsForAll/Capstone3/Models/ProductModel.java
@@ -1,4 +1,71 @@
 package com.AllForPets.PetsForAll.Capstone3.Models;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Document
 public class ProductModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long productID;
+    String productName;
+    String productDescription;
+    int productQuantity;
+    Double productPrice;
+    Long productSKU;
+
+    public ProductModel(String productName, String description, int quantity, Double price, Long SKU) {
+        this.productName = productName;
+        this.productDescription = description;
+        this.productQuantity = quantity;
+        this.productPrice = price;
+        this.productSKU = SKU;
+    }
+
+    public Long getProductID() {
+        return productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public Long getProductSKU() {
+        return productSKU;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductDescription(String description) {
+        this.productDescription = description;
+    }
+
+    public void setProductQuantity(int quantity) {
+        this.productQuantity = quantity;
+    }
+
+    public void setProductPrice(Double price) {
+        this.productPrice = price;
+    }
+
+    public void setProductSKU(Long SKU) {
+        this.productSKU = SKU;
+    }
 }
--- Capstone3/src/main/java/com/AllForPets/PetsForAll/Capstone3/dao/ProductRepository.java
+++ Capstone3/src/main/java/com/AllForPets/PetsForAll/Capstone3/dao/ProductRepository.java
@@ -1,6 +1,12 @@
 package com.AllForPets.PetsForAll.Capstone3.dao;
 
+import com.AllForPets.PetsForAll.Capstone3.Models.ProductModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends PagingAndSortingRepository {
+@Repository
+public interface ProductRepository extends MongoRepository<ProductModel, String> {
+    public ProductModel findByProductName(String productName);
+    public ProductModel findBySKU(Long productSKU);
 }
